3da175d614b472e1305cecd510d999db970a58d7
AndrewBloodworth <andrewcbld@gmail.com>
2021-02-21 21:15:20 -0700

    Button is connected with JS, need lotst more work

---
+++ Resources/JS/main.js
@@ -0,0 +1,11 @@
+
+const callback = (event) => {
+    
+}
+
+
+let button = document.getElementById("button");
+
+button.onclick = event => {
+    document.getElementById("post").innerHTML = "<h1>Hello World</h1>"
+}

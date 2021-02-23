98b236e9001135d6844b4fac413091d5db3ff861
AndrewBloodworth <andrewcbld@gmail.com>
2021-02-23 00:08:42 -0700

    Guarding DOM events

--- Resources/JS/main.js
+++ Resources/JS/main.js
@@ -8,42 +8,46 @@ if (document.getElementById("button")) {
 }
 
 //Adding Mouse Over and Mouse Out DOM events to the dombox of the Mind page.
-let boxes = document.getElementsByClassName("dombox");
+if (document.getElementsByClassName("dombox")[0]) {
+    let boxes = document.getElementsByClassName("dombox");
+    const boxMouseOver = event => {
+        let targetStyle = event.currentTarget.style;
+        targetStyle.backgroundColor = "turquoise";
+        targetStyle.width = "200px";
+        targetStyle.height = "200px";
+    };
+    const boxMouseOut = event => {
+        let targetStyle = event.currentTarget.style;
+        targetStyle.backgroundColor = "Dodgerblue";
+        targetStyle.width = "100px";
+        targetStyle.height = "100px";
+    };
+    for (let b in boxes) {
+        let box = boxes.item(b);
+        box.onmouseover = boxMouseOver;
+        box.onmouseout = boxMouseOut;
+    };
+};
 
-const boxMouseOver = event => {
-    let targetStyle = event.currentTarget.style;
-    targetStyle.backgroundColor = "turquoise";
-    targetStyle.width = "200px";
-    targetStyle.height = "200px";
-}
-const boxMouseOut = event => {
-    let targetStyle = event.currentTarget.style;
-    targetStyle.backgroundColor = "Dodgerblue";
-    targetStyle.width = "100px";
-    targetStyle.height = "100px";
-}
-for (let b in boxes) {
-    let box = boxes.item(b);
-    box.onmouseover = boxMouseOver;
-    box.onmouseout = boxMouseOut;
-}
 
 //Adding Mouse Over and Mouse Out DOM events to the Main-Text of the Banner
-let mainText = document.getElementsByClassName("main-text")[0];
 
-const mainMouseOver = event => {
-    let targetStyle = event.currentTarget.style;
-    targetStyle.backgroundColor = "black";
-    targetStyle.color = "white";
-    console.log("Hello");
+if (document.getElementsByClassName("main-text")[0]) {
+    let mainText = document.getElementsByClassName("main-text")[0];
+    const mainMouseOver = event => {
+        let targetStyle = event.currentTarget.style;
+        targetStyle.backgroundColor = "black";
+        targetStyle.color = "white";
+        console.log("Hello");
+    };
+    const mainMouseOut = event => {
+        let targetStyle = event.currentTarget.style;
+        targetStyle.backgroundColor = "white";
+        targetStyle.color = "black";
+    };
+    mainText.onmouseout = mainMouseOut;
+    mainText.onmouseover = mainMouseOver;
 };
-const mainMouseOut = event => {
-    let targetStyle = event.currentTarget.style;
-    targetStyle.backgroundColor = "white";
-    targetStyle.color = "black";
-};
-mainText.onmouseout = mainMouseOut;
-mainText.onmouseover = mainMouseOver;
 
 
 

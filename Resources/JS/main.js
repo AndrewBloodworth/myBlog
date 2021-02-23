7bf36686c6cb34dfed95985321a4ed614d3b318e
AndrewBloodworth <andrewcbld@gmail.com>
2021-02-22 23:35:57 -0700

    Mouse over and Mouse out in Mind.html, General Updates

--- Resources/JS/main.js
+++ Resources/JS/main.js
@@ -1,7 +1,22 @@
 
+if (document.getElementById("button")) {
+    let button = document.getElementById("button");
 
-let button = document.getElementById("button");
+    button.onclick = event => {
+        document.getElementById("post").innerHTML += '<div id="inline-flex"><h1>Hello World</h1></div>'
+    };
+}
 
-button.onclick = event => {
-    document.getElementById("post").innerHTML = "<h1>Hello World</h1>"
+let boxes = document.getElementsByClassName("dombox");
+
+const boxMouseOver = event => {
+    event.currentTarget.style.backgroundColor = "turquoise";
+}
+const boxMouseOut = event => {
+    event.currentTarget.style.backgroundColor = "Dodgerblue";
+}
+for (let i=0; i<boxes.length;i++) {
+    let box = boxes.item(i);
+    box.onmouseover = boxMouseOver;
+    box.onmouseout = boxMouseOut;
 }
